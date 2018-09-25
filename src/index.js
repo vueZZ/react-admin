38c8332927f146ab2cbad0a78c818742793d619c
zhangzhuo <1224008217@qq.com>
2018-09-25 18:02:20 +0800

    fix add antd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import { Button, LocaleProvider } from 'antd';
+import { Button, LocaleProvider } from 'antd';
 // // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 // import zhCN from 'antd/lib/locale-provider/zh_CN';
 // import 'antd/dist/antd.css';
@@ -15,10 +15,11 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div></div>
+      <div>
+        <Button>ss</Button>
+      </div>
       // <LocaleProvider locale={zhCN}>
       //   <div>
-      //     <Button>ss</Button>
       //   </div>
       // </LocaleProvider>
     );
